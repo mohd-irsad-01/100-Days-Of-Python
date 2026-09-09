c3c3aa3c604ad38bb3703483dee0a2a515a861e0
mohdirsad6386-maker <mohdirsad6386@gmail.com>
2026-09-09 20:21:22 +0530

    Day 01 - for loop added

---
+++ Day-01-For-Loop/for_loop.py
@@ -0,0 +1,16 @@
+# Day 01 - Python For Loop Practice
+# Author: Mohd Irsad
+# Topic: Understanding for loop in Python
+
+print("--- For Loop Example ---")
+
+# printing 1 to 5
+for i in range(1,6):
+    print(f"Number is: {i}")
+
+
+print("\n--- Loop with List ---")
+fruits = ['Apple', 'Mango', 'Banana']
+for fruit in fruits:
+    print(f"I like {fruit}")
+    
